9c42dc1bb6392e798771fd90b37e6123cc9c6489
krish-12932 <krish2006bgmi@gmail.com>
2026-05-19 15:31:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -19,8 +19,8 @@ GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 
 # Web Server Config
 PORT = int(os.getenv("PORT", 8080))
-web_domain_env = os.getenv("WEB_DOMAIN", "")
-render_url_env = os.getenv("RENDER_EXTERNAL_URL", "")
+web_domain_env = os.getenv("WEB_DOMAIN", "https://itachi-ai-bot-yxak.onrender.com")
+render_url_env = os.getenv("RENDER_EXTERNAL_URL", "https://itachi-ai-bot-yxak.onrender.com")
 
 if web_domain_env and "your-app-name" not in web_domain_env and "your-deployed-domain" not in web_domain_env:
     WEB_DOMAIN = web_domain_env.rstrip("/")
