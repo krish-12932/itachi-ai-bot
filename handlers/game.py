f371f26794b05a9e65cb02322eb0957a919e3bf0
krish-12932 <krish2006bgmi@gmail.com>
2026-05-28 17:57:01 +0530

    d

--- handlers/game.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-
-async def game_handler(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    """Placeholder for future game feature"""
-    await update.message.reply_text(
-        "🎮 **Game Feature**\n\n🌑 *Coming Soon......*\n\nItachi is preparing a challenge for you. Stay tuned!",
-        parse_mode="Markdown"
-    )
