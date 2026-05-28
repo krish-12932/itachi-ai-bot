62232b3d4a67e468beb7ac0089d38e0420520638
krish-12932 <krish2006bgmi@gmail.com>
2026-05-28 19:29:17 +0530

    Update inline.py

--- keyboards/inline.py
+++ keyboards/inline.py
@@ -12,8 +12,8 @@ def get_join_keyboard():
 
 def get_plan_keyboard(domain: str, coin_code: str, coin_20_code: str, unlimited_code: str):
     keyboard = [
-        [InlineKeyboardButton("📺 Watch 1 Ad (10 Coins) - Fast", web_app=WebAppInfo(url=f"{domain}/?code={coin_code}&req=1&net=monetag"))],
-        [InlineKeyboardButton("📺 Watch 2 Ads (20 Coins) - Bonus", web_app=WebAppInfo(url=f"{domain}/?code={coin_20_code}&req=2&net=taddy"))],
-        [InlineKeyboardButton("📺 Watch 10 Ads (Unlimited)", web_app=WebAppInfo(url=f"{domain}/?code={unlimited_code}&req=10&net=monetag"))]
+        [InlineKeyboardButton("📺 Watch 1 Ad (10 Coins) - Fast", web_app=WebAppInfo(url=f"{domain}/?code={coin_code}&req=1"))],
+        [InlineKeyboardButton("📺 Watch 2 Ads (20 Coins) - Bonus", web_app=WebAppInfo(url=f"{domain}/?code={coin_20_code}&req=2"))],
+        [InlineKeyboardButton("📺 Watch 10 Ads (Unlimited)", web_app=WebAppInfo(url=f"{domain}/?code={unlimited_code}&req=10"))]
     ]
     return InlineKeyboardMarkup(keyboard)
