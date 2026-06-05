4ad18ea35270bfccfbcdf389ba37e694f9e6cebc
krish-12932 <krish2006bgmi@gmail.com>
2026-06-05 13:39:35 +0530

    D

--- database/connection.py
+++
@@ -1,9 +0,0 @@
-from supabase import create_client, Client
-from config import SUPABASE_URL, SUPABASE_KEY
-
-def get_supabase() -> Client:
-    if not SUPABASE_URL or not SUPABASE_KEY:
-        raise ValueError("SUPABASE_URL or SUPABASE_KEY is not set in environment variables")
-    return create_client(SUPABASE_URL, SUPABASE_KEY)
-
-supabase = get_supabase()
